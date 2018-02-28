dddc74d2331d92abd92a71b3231ed6e2b2ad1813
marcinliwen <liwen.marcin@gmail.com>
2018-02-28 15:06:39 +0100

    create repository

---
+++ app.js
@@ -0,0 +1,27 @@
+var menu = document.getElementsByClassName("menu_item");
+var arrowDown = document.getElementsByClassName("arrow_down");
+
+/*****show arrows under menu when active********/
+function showme(evt, name) {
+    var i;
+    for(i = 0; i < arrowDown.length; i++){
+        arrowDown[i].style.display = "none";
+    }
+
+    for (i = 0; i < menu.length; i++) {
+        menu[i].className = menu[i].className.replace(" active", "");
+    }
+    
+    document.getElementById(name).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+    document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
